101707b26423d4e53ed58b3862ff0a38dd279242
Stanislav Z <slipdoll@gmail.com>
2020-05-14 12:31:00 +0300

    tags model for post, templates for tag

--- blog/views.py
+++ blog/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 
 
 # Create your views here.
-from blog.models import Post
+from blog.models import Post, Tag
 
 
 def post_list(request):
@@ -14,3 +14,13 @@ def post_list(request):
 def post_detail(request, slug):
     post = Post.objects.get(slug__iexact=slug)
     return render(request, 'blog/post_detail.html', context={'post': post})
+
+
+def tags_list(request):
+    tags = Tag.objects.all()
+    return render(request, 'blog/tags_list.html', context={'tags': tags})
+
+
+def tag_detail(request, slug):
+    tag = Tag.objects.get(slug__iexact=slug)
+    return render(request, 'blog/tag_detail.html', context={'tag': tag})
--- venv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/stas/PycharmProjects/blogengine/venv/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
