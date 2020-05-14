97203218ae256722de22619bc9eb9691b43cb048
Stanislav Z <slipdoll@gmail.com>
2020-05-14 11:31:13 +0300

    post model and templates for post

--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,14 @@ from django.http import HttpResponse
 
 
 # Create your views here.
+from blog.models import Post
+
+
 def post_list(request):
-    n = ['stas', 'masha', 'oleg']
-    return render(request, 'blog/index.html', context={'names': n})
+    posts = Post.objects.all()
+    return render(request, 'blog/index.html', context={'posts': posts})
+
+
+def post_detail(request, slug):
+    post = Post.objects.get(slug__iexact=slug)
+    return render(request, 'blog/post_detail.html', context={'post': post})
