4bb05a2ceacec88fd35d70bd547f63fc608391c8
Stanislav Z <slipdoll@gmail.com>
2020-05-22 17:54:24 +0300

    create forms

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import View
 
 # Create your views here.
 from blog.models import Post, Tag
+from .forms import TagForm, PostForm
 from .utils import ObjectDetailMixin
 
 
@@ -24,3 +25,33 @@ def tags_list(request):
 class TagDetail(ObjectDetailMixin, View):
     model = Tag
     template = 'blog/tag_detail.html'
+
+
+class TagCreate(View):
+    def get(self, request):
+        form = TagForm
+        return render(request, 'blog/tag_create.html', context={'form': form})
+
+    def post(self, request):
+        bound_form = TagForm(request.POST)
+
+        if bound_form.is_valid():
+            new_tag = bound_form.save()
+            return redirect(new_tag)
+
+        return render(request, 'blog/tag_create.html', context={'form': bound_form})
+
+
+class PostCreate(View):
+    def get(self, request):
+        form = PostForm
+        return render(request, 'blog/post_create_form.html', context={'form': form})
+
+    def post(self, request):
+        bound_form = PostForm(request.POST)
+
+        if bound_form.is_valid():
+            new_post = bound_form.save()
+            return redirect(new_post)
+
+        return render(request, 'blog/post_create_form.html', context={'form': bound_form})
