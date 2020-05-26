e0bf2404993a029c1724bf00eb4309c1b67130b6
Stanislav Z <slipdoll@gmail.com>
2020-05-26 15:12:47 +0300

    post list pagination

--- blog/models.py
+++ blog/models.py
@@ -16,6 +16,9 @@ class Post(models.Model):
     tags = models.ManyToManyField('Tag', blank=True, related_name='posts')
     date_pub = models.DateTimeField(auto_now_add=True)
 
+    class Meta:
+        ordering = ['-date_pub']
+
     # генерит ссылку через name заданный в url
     def get_absolute_url(self):
         return reverse('post_detail_url', kwargs={'slug': self.slug})
@@ -39,6 +42,9 @@ class Post(models.Model):
 class Tag(models.Model):
     title = models.CharField(max_length=50)
     slug = models.SlugField(max_length=50, unique=True)
+
+    class Meta:
+        ordering = ['title']
 
     def get_absolute_url(self):
         return reverse('tag_detail_url', kwargs={'slug': self.slug})
