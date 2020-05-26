966f0a48f72df84c193c2a371a6df95bbfcd98da
Stanislav Z <slipdoll@gmail.com>
2020-05-26 11:45:17 +0300

    custom css

--- blog/utils.py
+++ blog/utils.py
@@ -11,7 +11,9 @@ class ObjectDetailMixin:
     def get(self, request, slug):
             obj = get_object_or_404(self.model, slug__iexact=slug)
             return render(request, self.template, context={
-                str(self.model.__name__).lower(): obj
+                str(self.model.__name__).lower(): obj,
+                'admin_object': obj,
+                'detail': True
             })
 
 
